61cfedd24495239a4eb4f6e68db6fd98efde181e
Michael Tezza <michaeltezza@Air-di-Michael.homenet.telecomitalia.it>
2023-05-15 01:12:54 +0200

    Add message.ts

---
+++ server/src/message.ts
@@ -0,0 +1,122 @@
+import { Prisma, PrismaClient, MessageType } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function getMessagesOfChat(chatId: string) {
+    const messages = await prisma.chat.findMany({
+      where: {
+        id: chatId,
+      }
+    });
+  
+    console.dir(messages, { depth: Infinity });
+    console.log("------------------");
+  
+    return messages;
+  }
+
+  async function getUltimateMessagesOfChat(chatId: string) {
+    const message = await prisma.chat.findFirst({
+      where: {
+        id: chatId,
+      }
+    });
+  
+    console.dir(message, { depth: Infinity });
+    console.log("------------------");
+  
+    return message;
+  }
+
+  async function createMessage(chatId: string, content: string, messageType: MessageType) {
+    //   create chat
+  const newMessage = await prisma.message.create({
+    data: {
+      text: content,
+      messageType: messageType,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      Chat: {
+        connect: {
+          id: chatId,
+        },
+      },
+    },
+  });
+
+  //   add message to chat
+  const updatedChat = await prisma.chat.update({
+    where: {
+      id: chatId,
+    },
+    data: {
+      messages: {
+        connect: {
+          id: newMessage.id,
+        },
+      },
+    },
+  });
+
+  console.dir(newMessage, { depth: Infinity });
+  console.log("Message created!");
+  console.log("------------------");
+
+  return newMessage.id;
+  }
+
+  async function getMessageById(messageId: string) {
+    const message = await prisma.message.findUnique({
+      where: {
+        id: messageId,
+      },
+    });
+  
+    console.dir(message, { depth: Infinity });
+    console.log("Message found!");
+    console.log("------------------");
+  
+    return message;
+  }
+
+  async function deleteMessage(messageId: string) {
+    //   remove message from users
+    await prisma.message.update({
+      where: {
+        id: messageId,
+      },
+      data: {
+        Chat: {
+            disconnect: true,
+        },
+      },
+    });
+  
+    //   delete chat
+    await prisma.message.delete({
+      where: {
+        id: messageId,
+      },
+    });
+  
+    console.log("Message deleted!");
+    console.log("------------------");
+  }
+  async function removeAllMessagesChat(chatId: string) {
+    await prisma.chat.deleteMany({
+        where: {
+            id: chatId,
+        },
+    });
+  
+    console.log("All messages removed");
+    console.log("------------------");
+  }
+
+  export {
+    getMessagesOfChat,
+    getUltimateMessagesOfChat,
+    createMessage,
+    getMessageById,
+    deleteMessage,
+    removeAllMessagesChat,
+  };
