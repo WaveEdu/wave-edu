e36580372bed0c193fd5eea0c522cd0008f0f642
Micahel Tezza <mtezza9@gmail.com>
2023-06-09 14:51:29 +0200

    Resolve bug

--- server/src/db/story.ts
+++ server/src/db/story.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Story, User, UserType, MessageType, Lezione, Compito, Sondaggio, Evento, Comunicazione} from "@prisma/client";
+import { PrismaClient, User, UserType, MessageType, Lezione, Compito, Sondaggio, Evento, Comunicazione} from "@prisma/client";
 
 const prisma = new PrismaClient();
 
