5884e1f8b659a84e69dcbe94e4ec03ecc6504baa
Samuele Mazzei <samumaz01@gmail.com>
2023-06-09 11:51:25 +0200

    theme setup

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+	import ThemeToggle from '../components/ThemeToggle.svelte';
+</script>
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button class="btn btn-neutral">Neutral</button>
+<button class="btn btn-primary">Primary</button>
+<button class="btn btn-secondary">Secondary</button>
+<button class="btn btn-accent">Accent (tertiary)</button>
+<button class="btn btn-ghost">Ghost</button>
+<button class="btn btn-link">Link</button>
+
+<ThemeToggle />
