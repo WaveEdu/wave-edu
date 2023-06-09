dc6d82be91422436d12fa5822096258aeebf3784
Tezza Michael <36481462+TezzaMichael@users.noreply.github.com>
2023-06-09 16:01:30 +0200

    Update story.ts

--- server/src/router/story.ts
+++ server/src/router/story.ts
@@ -8,10 +8,12 @@ import {
     getlastStoryController
 } from "../controllers";
 
+import { isAuthenticated, isTeacher } from "../middlewares";
+
 export default (router: express.Router) => {
     router.get("/story/:id", getStoryController);
     router.get("/laststory", getlastStoryController);
-    router.put("/story/:id", putStoryController);
-    router.delete("/story/:id", deleteStoryController);
-    router.post("/story", postStoryController);
+    router.put("/story/:id", isTeacher, putStoryController);
+    router.delete("/story/:id", isTeacher, deleteStoryController);
+    router.post("/story", isTeacher, postStoryController);
   };
