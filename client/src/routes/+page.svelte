67c1ef9c0befa5382c48dbd47309ef7a0d2080a6
L-Viky <vittoria.longo@studenti.unitn.it>
2023-06-10 16:55:05 +0200

    Update +page.svelte

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -2,9 +2,10 @@
 	import Phone from '../components/Phone.svelte';
 	import ThemeToggle from '../components/ThemeToggle.svelte';
 	let chatTitle = 'Chat';
+	let nameApp = 'WaveWdu';
 </script>
 
-<h1 class="text-3xl font-bold underline">Wave Edu</h1>
+<h1 class="text-3xl font-bold underline">{nameApp}veEdu</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <button class="btn btn-neutral">Neutral</button>
@@ -20,31 +21,16 @@
 	<div class="flex h-full w-full flex-col">
 		<header>
 			<div class="navbar bg-base-100 space-x-2">
-				<div class="flex-none">
-					<a href="/" class="btn btn-square btn-ghost">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="1em"
-							height="1em"
-							viewBox="0 0 24 24"
-							class="inline-block h-5 w-5 stroke-current"
-							><path
-								fill="currentColor"
-								d="M20.25 12a.75.75 0 0 1-.75.75H6.31l5.47 5.47a.75.75 0 1 1-1.06 1.06l-6.75-6.75a.75.75 0 0 1 0-1.06l6.75-6.75a.75.75 0 1 1 1.06 1.06l-5.47 5.47H19.5a.75.75 0 0 1 .75.75Z"
-							/></svg
-						>
-					</a>
-				</div>
-				<div class="flex flex-1 flex-row gap-2">
+				<div class="my-6 flex flex-1 flex-row gap-2">
 					<div class="avatar">
-						<div class="mask mask-squircle w-10">
+						<div class="w-10 rounded-xl">
 							<img
-								src={`https://api.dicebear.com/6.x/initials/svg?seed=${chatTitle}`}
-								alt={chatTitle}
+							src={`https://api.dicebear.com/6.x/initials/svg?seed=${nameApp}`}
+							alt={nameApp}
 							/>
 						</div>
-					</div>
-					<h1 class="inline-block text-xl normal-case">{chatTitle}</h1>
+					  </div>
+					<h1 class="inline-block text-xl normal-case">WaveEdu</h1>
 				</div>
 				<div class="flex-none">
 					<button class="btn btn-square btn-ghost">
@@ -94,7 +80,7 @@
 </Phone>
 
 <Phone>
-	<div class="flex h-full w-full flex-col">
+	<div class="my-6 flex h-full w-full flex-col">
 		<header>
 			<div class="navbar bg-base-100 space-x-2">
 				<div class="flex-none">
