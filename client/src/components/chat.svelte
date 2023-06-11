12a3745d9af45844c44892a075a7ddd0f77c31a6
L-Viky <vittoria.longo@studenti.unitn.it>
2023-06-11 23:29:27 +0200

    Update Ui

---
+++ client/src/components/chat.svelte
@@ -0,0 +1,14 @@
+<div class="chat chat-start">
+    <div class="chat-header">Professor</div>
+    <div class="chat-bubble">..............</div>
+    <div class="chat-footer opacity-50">
+        <time class="text-xs opacity-50">2 hours ago</time>
+    </div>
+</div>
+<div class="chat chat-end">
+    <div class="chat-header">Student</div>
+    <div class="chat-bubble">..............</div>
+    <div class="chat-footer opacity-50">
+        <time class="text-xs">now</time>
+    </div>
+</div>
