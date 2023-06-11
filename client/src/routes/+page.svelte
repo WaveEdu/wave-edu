04e7a052d413bd584db1548ba5a36899cefc1652
L-Viky <vittoria.longo@studenti.unitn.it>
2023-06-11 19:39:07 +0200

    Update

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -89,8 +89,8 @@
 								/></svg
 							></summary>
 						<ul class="shadow menu dropdown-content bg-white rounded-box w-52">
-							<button class="rounded-box border-gray-300 border-4 hover:border-gray-400 p-1 text-center">Logout</button>
-							<button class="mt-1 rounded-box border-gray-300 border-4 hover:border-gray-400 p-1 text-center">Aggiungi Storia</button>
+							<button class="rounded-box border-gray-200 border-4 hover:border-gray-300 p-1 text-center">Logout</button>
+							<button class="mt-1 rounded-box border-gray-200 border-4 hover:border-gray-300 p-1 text-center">Aggiungi Storia</button>
 						</ul>
 					</details>
 				</div>
@@ -158,10 +158,9 @@
 				  />
                 </div>
               </div>
-
 		</header>
-		<main>
-			  <div class="mt-2 overflow-y-scroll">
+		<main class="flex h-full flex-col justify-start">
+			  <div class="overflow-y-scroll overflow-hidden ">
 				<table class="table">
 				  <tbody>
 					<!-- row 1 -->
@@ -170,12 +169,14 @@
 						<div class="flex items-center space-x-3">
 						  <div class="avatar">
 							<div class="mask mask-squircle w-12 h-12">
-							  <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+								<img
+								src={`https://api.dicebear.com/6.x/initials/svg?seed=${chatTitle}`}
+								alt={chatTitle}
+							/>
 							</div>
 						  </div>
 						  <div>
-							<div class="font-bold">Hart Hagerty</div>
-							<div class="text-sm opacity-50">United States</div>
+							<div class="font-bold">{chatTitle}</div>
 						  </div>
 						</div>
 					  </td>
@@ -189,12 +190,14 @@
 						<div class="flex items-center space-x-3">
 						  <div class="avatar">
 							<div class="mask mask-squircle w-12 h-12">
-							  <img src="/tailwind-css-component-profile-3@56w.png" alt="Avatar Tailwind CSS Component" />
+								<img
+								src={`https://api.dicebear.com/6.x/initials/svg?seed=${chatTitle}`}
+								alt={chatTitle}
+							/>
 							</div>
 						  </div>
 						  <div>
-							<div class="font-bold">Brice Swyre</div>
-							<div class="text-sm opacity-50">China</div>
+							<div class="font-bold">{chatTitle}</div>
 						  </div>
 						</div>
 					  </td>
@@ -208,12 +211,14 @@
 						<div class="flex items-center space-x-3">
 						  <div class="avatar">
 							<div class="mask mask-squircle w-12 h-12">
-							  <img src="/tailwind-css-component-profile-4@56w.png" alt="Avatar Tailwind CSS Component" />
+								<img
+								src={`https://api.dicebear.com/6.x/initials/svg?seed=${chatTitle}`}
+								alt={chatTitle}
+							/>
 							</div>
 						  </div>
 						  <div>
-							<div class="font-bold">Marjy Ferencz</div>
-							<div class="text-sm opacity-50">Russia</div>
+							<div class="font-bold">{chatTitle}</div>
 						  </div>
 						</div>
 					  </td>
@@ -227,12 +232,14 @@
 						<div class="flex items-center space-x-3">
 						  <div class="avatar">
 							<div class="mask mask-squircle w-12 h-12">
-							  <img src="/tailwind-css-component-profile-5@56w.png" alt="Avatar Tailwind CSS Component" />
+								<img
+								src={`https://api.dicebear.com/6.x/initials/svg?seed=${chatTitle}`}
+								alt={chatTitle}
+							/>
 							</div>
 						  </div>
 						  <div>
-							<div class="font-bold">Yancy Tear</div>
-							<div class="text-sm opacity-50">Brazil</div>
+							<div class="font-bold">{chatTitle}</div>
 						  </div>
 						</div>
 					  </td>
@@ -246,12 +253,56 @@
 						  <div class="flex items-center space-x-3">
 							<div class="avatar">
 							  <div class="mask mask-squircle w-12 h-12">
-								<img src="/tailwind-css-component-profile-3@56w.png" alt="Avatar Tailwind CSS Component" />
+								<img
+								src={`https://api.dicebear.com/6.x/initials/svg?seed=${chatTitle}`}
+								alt={chatTitle}
+							/>
 							</div>
 							</div>
 							<div>
-							  <div class="font-bold">Brice Swyre</div>
-							  <div class="text-sm opacity-50">China</div>
+								<div class="font-bold">{chatTitle}</div>
+							</div>
+						  </div>
+						</td>
+						<td>
+							...
+						</td>
+					</tr>
+					<!-- row 6 -->
+					<tr>
+						<td>
+						  <div class="flex items-center space-x-3">
+							<div class="avatar">
+							  <div class="mask mask-squircle w-12 h-12">
+								<img
+								src={`https://api.dicebear.com/6.x/initials/svg?seed=${chatTitle}`}
+								alt={chatTitle}
+							/>
+							</div>
+							</div>
+							<div>
+								<div class="font-bold">{chatTitle}</div>
+							</div>
+						  </div>
+						</td>
+						<td>
+							...
+						</td>
+					</tr>
+					<!-- row 7 -->
+					<tr>
+						<td>
+						  <div class="flex items-center space-x-3">
+							<div class="avatar">
+							  <div class="mask mask-squircle w-12 h-12">
+								<img
+								src={`https://api.dicebear.com/6.x/initials/svg?seed=${chatTitle}`}
+								alt={chatTitle}
+							/>
+							</div>
+							</div>
+							<div>
+								<div class="font-bold">{chatTitle}</div>
 							</div>
 						  </div>
 						</td>
@@ -262,6 +313,20 @@
 				  </tbody>
 				</table>
 			  </div>
+			  <div class="navbar m-1 bg-base-100 ">
+				<div class="mx-24 footer flex flex-row gap-20">
+					<div class="indicator">
+						<span class="indicator-item badge bg-slate-200 border-gray-200">+5</span> 
+						<button>
+							<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256"><path fill="currentColor" d="M236.06 187.69A84 84 0 0 0 172.29 68.9a84 84 0 1 0-152.35 70.79l-7.24 25.36A18 18 0 0 0 35 187.3l25.36-7.24a84.27 84.27 0 0 0 23.36 7a84.05 84.05 0 0 0 112 41l25.36 7.24a18 18 0 0 0 22.25-22.25Zm-207.7-23.47Zm33.17-9a12 12 0 0 0-3.3.46l-19.49 5.57l5.57-19.49a12 12 0 0 0-1-9.05a60 60 0 1 1 24 24a11.91 11.91 0 0 0-5.78-1.48Zm150.16 34.54l5.57 19.49l-19.49-5.57a12 12 0 0 0-9.05 1A60.06 60.06 0 0 1 111 186.63a83.93 83.93 0 0 0 68.55-91.37a60 60 0 0 1 33.16 85.46a12 12 0 0 0-1.02 9.05Z"/></svg>
+							<h4 class="inline-block text-sm normal-case">Chat</h4>					  
+						</button>
+					</div>
+					<button class="active">
+						<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M5.85 17.1q1.275-.975 2.85-1.538T12 15q1.725 0 3.3.563t2.85 1.537q.875-1.025 1.363-2.325T20 12q0-3.325-2.337-5.663T12 4Q8.675 4 6.337 6.337T4 12q0 1.475.488 2.775T5.85 17.1ZM12 13q-1.475 0-2.488-1.012T8.5 9.5q0-1.475 1.012-2.488T12 6q1.475 0 2.488 1.012T15.5 9.5q0 1.475-1.012 2.488T12 13Zm0 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z"/></svg>					
+					</button>
+				</div>
+			</div>
 		</main>
 		<footer>
 			<div class="navbar m-1 bg-base-100 ">
@@ -404,10 +469,11 @@
 									d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
 								/></svg
 							></summary>
-						<ul class="shadow menu dropdown-content bg-slate-200  rounded-box w-52">
-						  <button class=" text-center">Logout</button>
-						</ul>
-					</details>
+							<ul class="shadow menu dropdown-content bg-white rounded-box w-52">
+								<button class="rounded-box border-gray-200 border-4 hover:border-gray-300 p-1 text-center">Logout</button>
+								<button class="mt-1 rounded-box border-gray-200 border-4 hover:border-gray-300 p-1 text-center">Aggiungi Storia</button>
+							</ul>
+						</details>
 				</div>
 			</div>
 			<!-- Stories --> 
