14d291533b297222d0a24a79605849eb9507b31a
L-Viky <vittoria.longo@studenti.unitn.it>
2023-06-11 15:44:10 +0200

    Update +page.svelte (logo e stories)

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -63,7 +63,7 @@
 					<div class="avatar">
 						<div class="w-10 rounded-xl">
 							<img
-							src={'https://www.figma.com/file/tkf9qniauAJcAbHwEiHtdv/Logo-e-Mockup?type=design&node-id=50-6340&t=lmSWYFbWdq2Xzi3Q-4'}
+							src="/logo.svg"
 							/>
 						</div>
 					  </div>
@@ -89,43 +89,43 @@
 		</header>
 		<main class="flex h-full flex-col justify-start">
 			<!-- Stories --> 
-            <!-- devo capire come inserire le stories ()-->
+            <!-- devo capire come inserire le stories (gli scr son)-->
             <div class="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box">
                 <div class="carousel-item">
-                  <img src="/images/stock/photo-1559703248-dcaaec9fab78.jpg" class="rounded-box" />
+                  <img src={'https://placehold.co/100'} class="rounded-box" />
                 </div> 
                 <div class="carousel-item">
-                  <img src="/images/stock/photo-1565098772267-60af42b81ef2.jpg" class="rounded-box" />
+                  <img src={'https://placehold.co/100'} class="rounded-box" />
                 </div> 
                 <div class="carousel-item">
-                  <img src="/images/stock/photo-1572635148818-ef6fd45eb394.jpg" class="rounded-box" />
+                  <img src={'https://placehold.co/100'} class="rounded-box" />
                 </div> 
                 <div class="carousel-item">
-                  <img src="/images/stock/photo-1494253109108-2e30c049369b.jpg" class="rounded-box" />
+                  <img src={'https://placehold.co/100'} class="rounded-box" />
                 </div> 
                 <div class="carousel-item">
-                  <img src="/images/stock/photo-1550258987-190a2d41a8ba.jpg" class="rounded-box" />
+                  <img src={'https://placehold.co/100'} class="rounded-box" />
                 </div> 
                 <div class="carousel-item">
-                  <img src="/images/stock/photo-1559181567-c3190ca9959b.jpg" class="rounded-box" />
+                  <img src={'https://placehold.co/100'} class="rounded-box" />
                 </div> 
                 <div class="carousel-item">
-                  <img src="/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" class="rounded-box" />
+                  <img src={'https://placehold.co/100'} class="rounded-box" />
                 </div>
                 <div class="carousel-item">
-                    <img src="/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" class="rounded-box" />
+                  <img src={'https://placehold.co/100'} class="rounded-box" />
                 </div>
                  <div class="carousel-item">
-                    <img src="/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" class="rounded-box" />
+                  <img src={'https://placehold.co/100'} class="rounded-box" />
                 </div>
                 <div class="carousel-item">
-                    <img src="/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" class="rounded-box" />
+                  <img src={'https://placehold.co/100'} class="rounded-box" />
                 </div>
                 <div class="carousel-item">
-                    <img src="/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" class="rounded-box" />
+            	  <img src={'https://placehold.co/100'} class="rounded-box" />
                 </div>
                 <div class="carousel-item">
-                    <img src="/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" class="rounded-box" />
+                  <img src={'https://placehold.co/100'} class="rounded-box" />
                 </div>
               </div>
 			  <div class="mt-5 grid grid-flow-row gap-4">
@@ -143,12 +143,12 @@
 					<div class="indicator">
 						<span class="indicator-item badge badge-neutral">+5</span> 
 						<button>
-							<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256"><path fill="#000000" d="M236.06 187.69A84 84 0 0 0 172.29 68.9a84 84 0 1 0-152.35 70.79l-7.24 25.36A18 18 0 0 0 35 187.3l25.36-7.24a84.27 84.27 0 0 0 23.36 7a84.05 84.05 0 0 0 112 41l25.36 7.24a18 18 0 0 0 22.25-22.25Zm-207.7-23.47Zm33.17-9a12 12 0 0 0-3.3.46l-19.49 5.57l5.57-19.49a12 12 0 0 0-1-9.05a60 60 0 1 1 24 24a11.91 11.91 0 0 0-5.78-1.48Zm150.16 34.54l5.57 19.49l-19.49-5.57a12 12 0 0 0-9.05 1A60.06 60.06 0 0 1 111 186.63a83.93 83.93 0 0 0 68.55-91.37a60 60 0 0 1 33.16 85.46a12 12 0 0 0-1.02 9.05Z"/></svg>
+							<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256"><path fill="currentColor" d="M236.06 187.69A84 84 0 0 0 172.29 68.9a84 84 0 1 0-152.35 70.79l-7.24 25.36A18 18 0 0 0 35 187.3l25.36-7.24a84.27 84.27 0 0 0 23.36 7a84.05 84.05 0 0 0 112 41l25.36 7.24a18 18 0 0 0 22.25-22.25Zm-207.7-23.47Zm33.17-9a12 12 0 0 0-3.3.46l-19.49 5.57l5.57-19.49a12 12 0 0 0-1-9.05a60 60 0 1 1 24 24a11.91 11.91 0 0 0-5.78-1.48Zm150.16 34.54l5.57 19.49l-19.49-5.57a12 12 0 0 0-9.05 1A60.06 60.06 0 0 1 111 186.63a83.93 83.93 0 0 0 68.55-91.37a60 60 0 0 1 33.16 85.46a12 12 0 0 0-1.02 9.05Z"/></svg>
 							<h4 class="inline-block text-sm normal-case">Chat</h4>					  
 						</button>
 					</div>
 					<button class="active">
-						<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#000000" d="M5.85 17.1q1.275-.975 2.85-1.538T12 15q1.725 0 3.3.563t2.85 1.537q.875-1.025 1.363-2.325T20 12q0-3.325-2.337-5.663T12 4Q8.675 4 6.337 6.337T4 12q0 1.475.488 2.775T5.85 17.1ZM12 13q-1.475 0-2.488-1.012T8.5 9.5q0-1.475 1.012-2.488T12 6q1.475 0 2.488 1.012T15.5 9.5q0 1.475-1.012 2.488T12 13Zm0 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z"/></svg>					
+						<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M5.85 17.1q1.275-.975 2.85-1.538T12 15q1.725 0 3.3.563t2.85 1.537q.875-1.025 1.363-2.325T20 12q0-3.325-2.337-5.663T12 4Q8.675 4 6.337 6.337T4 12q0 1.475.488 2.775T5.85 17.1ZM12 13q-1.475 0-2.488-1.012T8.5 9.5q0-1.475 1.012-2.488T12 6q1.475 0 2.488 1.012T15.5 9.5q0 1.475-1.012 2.488T12 13Zm0 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z"/></svg>					
 					</button>
 				</div>
 			</div>
