929eab32a03d54cec2d2eeb3ac0587f37f2a74f4
L-Viky <vittoria.longo@studenti.unitn.it>
2023-06-11 13:28:29 +0200

    Update +page.svelte (warning)

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -46,10 +46,11 @@
 			<div class="mx-4 mt-5 w-full join">
 				<input class="input w-80 join-item rounded-sm outline" placeholder="Email"/>
 			</div>
-			<div class="alert mt-5 h-11 alert-warning">
+			<div class="alert mt-5 h-14 grid-cols-2 bg-amber-500">
 				<svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+				<span>Invalid email address!</span>
 			</div>
-			<button class="btn btn-neutral w-full mt-5">Accedi</button>
+			<!--<button class="btn btn-neutral w-full mt-5">Accedi</button>-->
 		</main>
 	</div>
 </Phone>
@@ -58,7 +59,7 @@
 	<div class="flex h-full w-full flex-col">
 		<header>
 			<div class="navbar bg-base-100 space-x-2">
-				<div class="my-6 flex flex-1 flex-row gap-2">
+				<div class="my-8 flex flex-1 flex-row gap-2">
 					<div class="avatar">
 						<div class="w-10 rounded-xl">
 							<img
@@ -127,23 +128,27 @@
                     <img src="/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" class="rounded-box" />
                 </div>
               </div>
-			  <div class="grid grid-flow-row gap-4">
-				<div class>01{chatTitle}</div>
-				<!-- ... -->
-				<div>09</div>
+			  <div class="mt-5 grid grid-flow-row gap-4">
+				<div class>{chatTitle}</div>
+				<div class>{chatTitle}</div>
+				<div class>{chatTitle}</div>
+				<div class>{chatTitle}</div>
+				<div class>{chatTitle}</div>
+				<div>{chatTitle}</div>
 			  </div>
 		</main>
 		<footer>
 			<div class="navbar m-1 bg-base-100 ">
 				<div class="mx-24 footer flex flex-row gap-20">
 					<div class="indicator">
-						<span class="indicator-item badge badge-neutral"></span> 
+						<span class="indicator-item badge badge-neutral">+5</span> 
 						<button>
-						<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256"><path fill="#000000" d="M236.06 187.69A84 84 0 0 0 172.29 68.9a84 84 0 1 0-152.35 70.79l-7.24 25.36A18 18 0 0 0 35 187.3l25.36-7.24a84.27 84.27 0 0 0 23.36 7a84.05 84.05 0 0 0 112 41l25.36 7.24a18 18 0 0 0 22.25-22.25Zm-207.7-23.47Zm33.17-9a12 12 0 0 0-3.3.46l-19.49 5.57l5.57-19.49a12 12 0 0 0-1-9.05a60 60 0 1 1 24 24a11.91 11.91 0 0 0-5.78-1.48Zm150.16 34.54l5.57 19.49l-19.49-5.57a12 12 0 0 0-9.05 1A60.06 60.06 0 0 1 111 186.63a83.93 83.93 0 0 0 68.55-91.37a60 60 0 0 1 33.16 85.46a12 12 0 0 0-1.02 9.05Z"/></svg>					  
+							<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256"><path fill="#000000" d="M236.06 187.69A84 84 0 0 0 172.29 68.9a84 84 0 1 0-152.35 70.79l-7.24 25.36A18 18 0 0 0 35 187.3l25.36-7.24a84.27 84.27 0 0 0 23.36 7a84.05 84.05 0 0 0 112 41l25.36 7.24a18 18 0 0 0 22.25-22.25Zm-207.7-23.47Zm33.17-9a12 12 0 0 0-3.3.46l-19.49 5.57l5.57-19.49a12 12 0 0 0-1-9.05a60 60 0 1 1 24 24a11.91 11.91 0 0 0-5.78-1.48Zm150.16 34.54l5.57 19.49l-19.49-5.57a12 12 0 0 0-9.05 1A60.06 60.06 0 0 1 111 186.63a83.93 83.93 0 0 0 68.55-91.37a60 60 0 0 1 33.16 85.46a12 12 0 0 0-1.02 9.05Z"/></svg>
+							<h4 class="inline-block text-sm normal-case">Chat</h4>					  
 						</button>
 					</div>
 					<button class="active">
-					  <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+						<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#000000" d="M5.85 17.1q1.275-.975 2.85-1.538T12 15q1.725 0 3.3.563t2.85 1.537q.875-1.025 1.363-2.325T20 12q0-3.325-2.337-5.663T12 4Q8.675 4 6.337 6.337T4 12q0 1.475.488 2.775T5.85 17.1ZM12 13q-1.475 0-2.488-1.012T8.5 9.5q0-1.475 1.012-2.488T12 6q1.475 0 2.488 1.012T15.5 9.5q0 1.475-1.012 2.488T12 13Zm0 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z"/></svg>					
 					</button>
 				</div>
 			</div>
